c89bebfdf602f243fb717cc36429bde455417f96
sk06-source <sharan.6022@gmail.com>
2024-07-18 13:18:19 +0530

    Create main.java

---
+++ main.java
@@ -0,0 +1,35 @@
+public class main
+{
+public static void main(String[] args)
+{
+/*int a = 9;
+double d = a;
+System.out.println(a);      														/s
+System.out.println(d);
+}
+}
+
+
+int sum1 = 100 + 50;
+int sum2 = sum1 + 250;
+int sum3 = sum2 + sum2;
+System.out.print(sum3);
+}
+}
+
+
+int x, y;
+x = 20;
+y = (x == 1) ? 61 : 90;
+System.out.println("Value of y is: " + y);
+y = (x == 20)? 61:90;
+System.out.println("Value of y is: " + y);
+}
+}
+
+
+String txt = "QWERTYUIOPLKJHGFDSAZXCVBNM";
+System.out.println("The length of the text string is:  " + txt.length());
+}
+}
+*/
